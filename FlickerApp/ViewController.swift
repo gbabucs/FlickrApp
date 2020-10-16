3b345fef536c0fc01daa2ab02c6d034719a4570b
Babu Gangatharan <babu.gangatharan@sogeti.com>
2020-10-16 18:04:15 +0200

    Initial Commit

---
+++ FlickerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickerApp
+//
+//  Created by Babu on 16/10/2020.
+//  Copyright © 2020 system. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
